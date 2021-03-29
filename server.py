540ff8d762a99c7c2b587f85012b505c2bd9cef4
Derek Carr <dcarr@ancestry.com>
2021-03-29 11:44:50 -0600

    Websockets, new ui control layout, rotating

--- server.py
+++ server.py
@@ -6,17 +6,45 @@ import os
 import urllib.parse
 from flask.json import jsonify
 from configparser import ConfigParser
-
-from os import listdir
-from os.path import isfile, join
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+import random
+from flask_socketio import send, emit, disconnect, SocketIO
+import sqlite3
 
 
 os.environ["DISPLAY"] = ":0"
 TEMPLATES_AUTO_RELOAD = True
 
+# con = sqlite3.connect('settings.sqlite')
+# cur = con.cursor()
+# t = '''
+# CREATE TABLE IF NOT EXISTS run_mode (
+#     id INTEGER PRIMARY KEY,
+#     mode TEXT NOT NULL,
+#     active INTEGER DEFAULT 0
+# );'''
+# cur.execute(t)
+## Needs to be mutable
+run_mode_images = []
+index = [0]
+scheduler
 
 app = Flask(__name__, template_folder='templates')
+socketio = SocketIO(app)
 
+@socketio.on('currentImage')
+def handle_message(data):
+    print('received message: ' + data)
+
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
 
 @app.route('/', methods=['GET'])
 def root():
@@ -24,22 +52,14 @@ def root():
     # return send_from_directory("static", "index.html")
     return render_template('index.html') # Return index.html
 
+
 @app.route('/navigation.html', methods=['GET'])
 def nav():
     return render_template('navigation.html') # Return index.html
 
-# @app.route('/cached', methods=['GET'])
-# def cached():
-#     return render_template('cached.html') # Return index.html
-
-
-# @app.route('/hello/<name>')
-# def hello(name=None):
-#     return render_template('hello.html', name=name)
-
-# @app.route('/<name>')
-# def hello_name(name):
-#     return "Hello {}!".format(name)
+@app.route('/poster/current', methods=["GET"])
+def get_current_poster():
+    return getImageConfig()
 
 @app.route('/poster/url', methods=['POST'])
 def set_poster():
@@ -55,22 +75,30 @@ def set_poster():
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw, f)
             print ("Saved file: ./static/images/" + url2)
+    setImageConfig(url2)
     launchImage(url2)
-    # img = Image.open("./static/images/" + url2)
-    # transposed = img.transpose(Image.ROTATE_270)
-    #
-    # transposed.save("img_t." + fileExtension)
-    # os.system('feh --hide-pointer --full-screen --zoom max "./img_t.{0}" &'.format(fileExtension))
-    # # return "Loading poster {}".format(url)
+    build_dynamic_run_mode()
     resp = jsonify(success=True)
     return resp
 
+@app.route('/poster/url/random')
+def set_poster_random():
+    imgpath = "./static/images"
+    images = []
+    for filename in os.listdir(imgpath):
+        images.append(filename)
+    index = random.randint(0, len(images) - 1)
+    setImageConfig(images[index])
+    launchImage(images[index])
+    return jsonify(success=True)
+
+
 @app.route('/poster/img/<img>', methods=['GET'])
 def get_poster(img):
+    setImageConfig(img)
     launchImage(img)
     resp = jsonify(success=True)
     return resp
-    #return "Loading poster {}".format(url)
 
 @app.route('/poster/img/<img>', methods=['DELETE'])
 def delete_poster(img):
@@ -92,44 +120,59 @@ def get_posters_cached():
     imgpath = "./static/images"
     images = []
     for filename in os.listdir(imgpath):
-        # images.append(os.path.join("/static/images", filename))
         images.append(filename)
-        # if filename.endswith(".jpg") and filename != "notapril.jpg":
-        #     allninjas.append(os.path.join('static/images', filename))
-        # else:
-        #     continue
-    # return jsonify(images)
     return render_template('cached.html', images=images)
-    # onlyfiles = [f for f in listdir(imgpath) if isfile(join(imgpath, f))]
-    # return os.listdir(imgpath)
 
 
-@app.route('/runmode', methods=['PUT'])
-def saveRunMode():
+@app.route('/runmode/<mode>/<value>', methods=['PUT'])
+def saveRunMode(mode, value):
     config = ConfigParser()
-
     config.read('config.ini')
-    config.add_section('runmode')
-    config.set('runmode', 'manual', 'value1')
-    config.set('runmode', 'random', 'value2')
-    config.set('runmode', 'random_rotating', 'value3')
-    config.set('runmode', 'plex', 'value3')
+    config.set('runmode', mode, value)
 
     with open('config.ini', 'w') as f:
         config.write(f)
+    # if (mode == "dynamic"):
+        # build_dynamic_run_mode()
     return jsonify(success=True)
 
 @app.route('/runmode', methods=['GET'])
 def getRunMode():
     config = ConfigParser()
-
     config.read('config.ini')
+    r = dict(config.items('runmode'))
+    return jsonify(r)
 
-    print(config.get('runmode', 'manual')) # -> "value1")
-    print(config.get('runmode', 'random'))  # -> "value2"
-    print(config.get('runmode', 'random_rotating'))  # -> "value3"
-    print(config.get('runmode', 'plex'))  # -> "value3"
+@app.route('/runmode/static', methods=['GET'])
+def getRunModeStatic():
+    config = ConfigParser()
+    config.read('config.ini')
+    r = dict(config.items('static'))
+    return jsonify(r)
 
+@app.route('/runmode/dynamic', methods=['GET'])
+def getRunModeDynamic():
+    config = ConfigParser()
+    config.read('config.ini')
+    r = dict(config.items('dynamic'))
+    return jsonify(r)
+
+@app.route('/runmode/dynamic/<mode>/<vaue>', methods=['PUT'])
+def setRunModeDynamic(mode, value):
+    config = ConfigParser()
+    config.read('config.ini')
+    config.set('dynamic', mode, value)
+
+    with open('config.ini', 'w') as f:
+        config.write(f)
+    build_dynamic_run_mode()
+    return jsonify(success=True)
+
+@app.route('/schedule/<interval>')
+def setScheduleInterval():
+    scheduler.shutdown()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=run_mode, args=[index], trigger="interval", seconds=300)
 
 def launchImage(filename):
     fileExtension = filename.rsplit('.', 1)[-1]
@@ -139,7 +182,73 @@ def launchImage(filename):
 
     transposed.save("img_t." + fileExtension)
     os.system('sudo killall feh; feh --hide-pointer --full-screen --zoom max "./img_t.{0}" &'.format(fileExtension))
+    socketio.emit('currentImage', filename)
+    setImageConfig(filename)
     return
+
+def setImageConfig(filename):
+    config = ConfigParser()
+    config.read('config.ini')
+    config.set('main', "current", filename)
+
+    with open('config.ini', 'w') as f:
+        config.write(f)
+
+def getImageConfig():
+    config = ConfigParser()
+    config.read('config.ini')
+    return config.get("main", "current")
+
+#####
+# Scheduled Tasks
+#####
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+def build_dynamic_run_mode():
+    index = 0
+    config = ConfigParser()
+    config.read('config.ini')
+    r = dict(config.items('dynamic'))
+    if (r['random_rotating'] == "true"):
+        imgpath = "./static/images"
+        run_mode_images.clear()
+        for filename in os.listdir(imgpath):
+            run_mode_images.append(filename)
+        print("Random Rotating Selected")
+
+
+
+## Reads next in list of ones to display
+## List gets built from the set run mode options
+def run_mode(index):
+    config = ConfigParser()
+    config.read('config.ini')
+    r = dict(config.items('runmode'))
+    if (r['mode'] == "static"):
+        print("STATIC ")
+        return
+    if (len(run_mode_images) < 1):
+        print("NOT ENOUGH")
+        return
+    else:
+        index[0] += 1
+        if (index[0] >= len(run_mode_images)):
+            index[0] = 0
+        current_image = run_mode_images[index[0]]
+        launchImage(current_image)
+
+    # Order of importance
+#INIT
+launchImage(getImageConfig())
+build_dynamic_run_mode()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=run_mode, args=[index], trigger="interval", seconds=300)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
